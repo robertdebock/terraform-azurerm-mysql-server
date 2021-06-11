14d9c541e7c851a7ad28f78af1f94f002bd5a75d
Robert de Bock <robert@meinit.nl>
2021-06-11 13:15:42 +0200

    Use same resources.

--- main.tf
+++ main.tf
@@ -20,6 +20,22 @@ resource "azurerm_mysql_server" "example" {
   geo_redundant_backup_enabled      = false
   # infrastructure_encryption_enabled = true
   public_network_access_enabled     = true
-  ssl_enforcement_enabled           = true
-  ssl_minimal_tls_version_enforced  = "TLS1_2"
+  ssl_enforcement_enabled           = false
+  # ssl_minimal_tls_version_enforced  = "TLS1_2"
+}
+
+resource "azurerm_mysql_firewall_rule" "example" {
+  name                = "AccessFromAnywhere"
+  resource_group_name = azurerm_resource_group.example.name
+  server_name         = azurerm_mysql_server.example.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
+
+resource "azurerm_mysql_database" "mysql-db" {
+  name                = "my_db"
+  resource_group_name = azurerm_resource_group.example.name
+  server_name         = azurerm_mysql_server.example.name
+  charset             = "utf8"
+  collation           = "utf8_unicode_ci"
 }
